4ee40e3d3ff4402c3da423128440bc30741bfab8
Joe Gurney <jgurnney@gmail.com>
2019-01-18 10:43:20 +0100

    created the 2 domain objects with pks and fk

--- src/main/java/com/qa/persistence/domain/Trainee.java
+++ src/main/java/com/qa/persistence/domain/Trainee.java
@@ -1,5 +1,40 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Trainee {
 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int traineeId;
+	private String traineeName;
+	
+	@ManyToOne
+	private int roomId;
+	
+	public Trainee() {
+		//meant to be empty
+	}
+	
+	public Trainee(String traineeName) {
+		setTraineeName(traineeName);
+	}
+
+	public String getTraineeName() {
+		return traineeName;
+	}
+
+	public void setTraineeName(String traineeName) {
+		this.traineeName = traineeName;
+	}
+
+	public int getTraineeId() {
+		return traineeId;
+	}
+
+	public int getRoomId() {
+		return roomId;
+	}
+	
 }

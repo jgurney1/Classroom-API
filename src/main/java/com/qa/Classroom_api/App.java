ba5ca3c52f66e80c116075d378689fbbf6f197ce
Joe Gurney <jgurnney@gmail.com>
2019-01-18 10:10:58 +0100

    Init

---
+++ src/main/java/com/qa/Classroom_api/App.java
@@ -0,0 +1,13 @@
+package com.qa.Classroom_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
